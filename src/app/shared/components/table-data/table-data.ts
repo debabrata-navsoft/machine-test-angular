adb09cf67e965088c9b09357ce623972b4d04272
debabrata-navsoft <debabrata.das@navsoft.in>
2026-09-16 19:27:02 +0530

    feat: add AuthService and associated tests; create HomePage and TableData components with tests; update index title

---
+++ src/app/shared/components/table-data/table-data.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-data',
+  imports: [],
+  templateUrl: './table-data.html',
+  styleUrl: './table-data.css',
+})
+export class TableData {}
